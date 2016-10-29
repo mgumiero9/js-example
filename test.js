dcd81a8a6412af403e8e52e1c06543a0fd30a3c7
mgumiero9 <mgumiero9@gmail.com>
2016-10-29 16:03:38 -0200

    basic functionality ok

--- test.js
+++ test.js
@@ -1,1 +1,17 @@
+var content = document.getElementById("content");
+var button = document.getElementById("show-more");
 
+button.onclick = function() {
+
+    if (content.className == "open") {
+        //shrink
+        content.className = "";
+        button.innerHTML = "Show More";
+
+    } else {
+        //increase
+        content.className = "open";
+        button.innerHTML = "Show Less";
+    }
+
+}
