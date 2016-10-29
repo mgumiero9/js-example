f9c7c670cf39d6a52f5d75e2e395a14c07a8effe
mgumiero9 <mgumiero9@gmail.com>
2016-10-29 19:22:04 -0200

    Adding window.onload functionality

--- test.js
+++ test.js
@@ -1,17 +1,20 @@
-var content = document.getElementById("content");
-var button = document.getElementById("show-more");
-
-button.onclick = function() {
-
-    if (content.className == "open") {
-        //shrink
-        content.className = "";
-        button.innerHTML = "Show More";
-
-    } else {
-        //increase
-        content.className = "open";
-        button.innerHTML = "Show Less";
+function setUpEvents() {
+    var content = document.getElementById("content");
+    var button = document.getElementById("show-more");
+    button.onclick = function () {
+        if (content.className == "open") {
+            //shrink
+            content.className = "";
+            button.innerHTML = "Show More";
+        }
+        else {
+            //increase
+            content.className = "open";
+            button.innerHTML = "Show Less";
+        }
     }
+}
 
+window.onload = function(){
+    setUpEvents();
 }
