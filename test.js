385e3baedfa2466266ecefe6820a5de9c6611267
mgumiero9 <mgumiero9@gmail.com>
2016-10-29 22:44:33 -0200

    Timer adjustments

--- test.js
+++ test.js
@@ -2,10 +2,13 @@ function setUpEvents() {
     var content = document.getElementById("content");
     var button = document.getElementById("show-more");
     var myMessage = document.getElementById("message");
+    var myColorChanger = document.getElementById('color-changer');
+    var colors = ['red','blue','pink','yellow'];
+    var counter = 0;
     function showMessage(){
         myMessage.className = 'show';
     }
-    setTimeout(showMessage, 3000);
+    setTimeout(showMessage, 2000);
     button.onclick = function () {
         if (content.className == "open") {
             //shrink
