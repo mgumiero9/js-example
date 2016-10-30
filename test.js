8b9fbb3ac2687b01308f278674e480a335354f3f
mgumiero9 <mgumiero9@gmail.com>
2016-10-30 10:34:33 -0200

    Adding timer interval function

--- test.js
+++ test.js
@@ -2,11 +2,12 @@ function setUpEvents() {
     var content = document.getElementById("content");
     var button = document.getElementById("show-more");
     var myMessage = document.getElementById("message");
-    var myColorChanger = document.getElementById('color-changer');
-    var colors = ['red','blue','pink','yellow'];
+    var myColorChanger = document.getElementById("color-changer");
+    var colors = ["red", "blue", "pink", "yellow"];
     var counter = 0;
-    function showMessage(){
-        myMessage.className = 'show';
+
+    function showMessage() {
+        myMessage.className = "show";
     }
     setTimeout(showMessage, 2000);
     button.onclick = function () {
@@ -20,6 +21,17 @@ function setUpEvents() {
             content.className = "open";
             button.innerHTML = "Show Less";
         }
+    }
+
+    function colorChanger() {
+        if (counter >= 4) counter = 0;
+        myColorChanger.style.background = colors[counter];
+        counter++;
+    }
+    var myInterval = setInterval(colorChanger, 2000);
+    myColorChanger.onclick = function () {
+        clearInterval(myInterval);
+        myColorChanger.innerHTML = 'TIMER STOPPED';
     }
 }
 window.onload = function () {
